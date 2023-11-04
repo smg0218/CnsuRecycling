ac16b49bf3030c2fadcc2a5672f73223ec41b697
YoungHeeSo <apdlfdldjqtm@gmail.com>
2023-11-04 15:27:35 +0900

    # 톰캣 설정

--- src/main/java/com/jsp/JspStarterMain.java
+++ src/main/java/com/jsp/JspStarterMain.java
@@ -1,6 +1,11 @@
-package src.main.java.com.jsp;
+package com.jsp;
 
-
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
 
 import java.io.File;
 
