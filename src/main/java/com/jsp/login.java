5360285a0b950eea2badf47aa042ac0d64f09c63
seon <darkness.blooms.sun@gmail.com>
2023-11-24 13:44:37 +0900

    db 연결 테스트 2차완 test 완료 페이지넘김 성공

--- src/main/java/com/jsp/login.java
+++ src/main/java/com/jsp/login.java
@@ -1,26 +1,27 @@
 package com.jsp;
 
-import db.DBConnect;
-
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import static db.DBConnect.select;
 
 
+@WebServlet("/ps_young/loginPro")
+public class login extends HttpServlet {
+    private static final long serialVersionUID = 1L;
 
-public class loginController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
 
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
         request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=utf-8");
 
@@ -31,10 +32,9 @@ public class loginController extends HttpServlet {
 
         }else{
             request.getSession().setAttribute("name",name);
+//            PrintWriter out = response.getWriter();
+//            out.print(name);
         }
 
-
     }
-
-
 }
